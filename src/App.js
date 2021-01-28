7d6490156a9ce3365f4ebcecbf82ab2887246b79
adiphoorn <a.diphoorn@novi-education.nl>
2021-01-28 23:36:44 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Navigation from './components/navigation/navigation';
+import Logo from './components/logo/logo';
+import ImageLinkForm from './components/imageLinkform/imageLinkform';
+import Rank from './components/rank/rank';
+import Particles from 'react-particles-js';
 import './App.css';
+import 'tachyons';
 
-function App() {
+const particleOptions = {
+    particles: {
+        number: {
+          value: 160,
+          density: {
+            enable: true,
+            value_area: 800
+          } 
+        },
+        color: {
+           value: "#333333",
+        }, 
+ }
+}
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Particles className="particles" 
+       params={particleOptions}
+                />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
     </div>
   );
+ }
 }
+
 
 export default App;
